1f5ab2ebd827aa999af77b2d1893b027dbd71de2
Atilla <mursalzadeh99@gmail.com>
2023-05-21 18:07:40 +0400

    Added neew branch

--- April/9/9.04/Functions.h
+++
@@ -1,3 +0,0 @@
-int max(int* num1, int* num2, int len1, int len2);
-int min(int* num1, int* num2, int len1, int len2);
-int avg(int* num1, int* num2, int len1, int len2);
--- March/30/30/Functions.h
+++
@@ -1,6 +0,0 @@
-int mystrcmp(char* str1, char* str2);
-void StringToNumber(char* string);
-void NumberToString(int* number);
-char* Uppercase(char* str1);
-char* Lowercase(char* str1);
-char* mystrrev(char* str);
