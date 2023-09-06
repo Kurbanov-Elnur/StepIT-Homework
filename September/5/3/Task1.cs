ccaa6867b7221ba798c46a9730d1ff5be99edcfb
Atilla <mursalzadeh99@gmail.com>
2023-09-06 19:23:29 +0400

    Add HW

---
+++ September/5/3/Task1.cs
@@ -0,0 +1,90 @@
+﻿//Journal journal = new Journal();
+
+//journal.inputData();
+//Console.WriteLine();
+//journal.printData();
+
+//class Journal
+//{
+//    public string? name { get; private set; }
+//    public int yearFounded { get; private set; }
+//    public string? description { get; private set; }
+//    public string? phone { get; private set; }
+//    public string? email { get; private set; }
+//    public int NumberOfEmployees { get; private set; }
+
+//    public void inputData()
+//    {
+//        Console.Write("Enter journal name: ");
+//        name = Console.ReadLine();
+
+//        Console.Write("Enter journal year founded: ");
+//        yearFounded = int.Parse(Console.ReadLine());
+
+//        Console.Write("Enter journal description: ");
+//        description = Console.ReadLine();
+
+//        Console.Write("Enter contact phone: ");
+//        phone = Console.ReadLine();
+
+//        Console.Write("Enter contact e-mail: ");
+//        email = Console.ReadLine();
+
+//        Console.WriteLine("Enter number of Employees: ");
+//        NumberOfEmployees = int.Parse(Console.ReadLine());
+//    }
+
+//    public void printData()
+//    {
+//        Console.WriteLine("Journal name: " + name);
+//        Console.WriteLine("Journal year founded: " + yearFounded);
+//        Console.WriteLine("Journal description: " + description);
+//        Console.WriteLine("Journal contact phone: " + phone);
+//        Console.WriteLine("Journal contact e-mail: " + email);
+//        Console.WriteLine("Journal number of employees " + NumberOfEmployees);
+//    }
+
+//    public static Journal operator +(Journal journal1, int count)
+//    {
+//        journal1.NumberOfEmployees += count;
+//        return journal1;
+//    }
+
+//    public static Journal operator -(Journal journal1, int count)
+//    {
+//        journal1.NumberOfEmployees -= count;
+//        return journal1;
+//    }
+
+//    public static bool operator >(Journal journal1, Journal journal2)
+//    {
+//        return journal1.NumberOfEmployees > journal2.NumberOfEmployees;
+//    }
+
+//    public static bool operator <(Journal journal1, Journal journal2)
+//    {
+//        return journal1.NumberOfEmployees < journal2.NumberOfEmployees;
+//    }
+
+//    public static bool operator ==(Journal journal1, Journal journal2)
+//    {
+//        return journal1.Equals(journal2);
+//    }
+
+//    public static bool operator !=(Journal journal1, Journal journal2)
+//    {
+//        return !journal1.Equals(journal2);
+//    }
+
+//    public override bool Equals(object obj)
+//    {
+//        if (obj is Journal) 
+//        {
+//            Journal j = obj as Journal;
+//            return j.name == name && j.yearFounded == yearFounded && j.description == description && j.phone == phone
+//                && j.email == email && j.NumberOfEmployees == NumberOfEmployees;
+//        }
+
+//        return false;
+//    }
+//}
---
+++ September/5/3/Task3.cs
@@ -0,0 +1,61 @@
+﻿//public class Book
+//{
+//    public string Title { get; set; }
+//    public string Author { get; set; }
+
+//    public Book(string title, string author)
+//    {
+//        Title = title;
+//        Author = author;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"{Title} by {Author}";
+//    }
+//}
+
+//public class BookList
+//{
+//    private List<Book> books;
+
+//    public BookList()
+//    {
+//        books = new List<Book>();
+//    }
+
+//    public void AddBook(Book book)
+//    {
+//        books.Add(book);
+//    }
+
+//    public void RemoveBook(Book book)
+//    {
+//        books.Remove(book);
+//    }
+
+//    public bool ContainsBook(Book book)
+//    {
+//        return books.Contains(book);
+//    }
+
+//    public Book this[int index]
+//    {
+//        get
+//        {
+//            if (index >= 0 && index < books.Count)
+//            {
+//                return books[index];
+//            }
+//            else
+//            {
+//                throw new IndexOutOfRangeException("Index is out of range.");
+//            }
+//        }
+//    }
+
+//    public int Count
+//    {
+//        get { return books.Count; }
+//    }
+//}
