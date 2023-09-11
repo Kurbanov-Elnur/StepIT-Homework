a89e545d3823d22135ee96a95d8b8d472fd5bbc5
Atilla <mursalzadeh99@gmail.com>
2023-09-11 16:49:03 +0400

    Add HW

---
+++ September/3-2/3-2/Task1.cs
@@ -0,0 +1,69 @@
+﻿//public class Money
+//{
+//    public int Dollars { get; private set; }
+//    public int Cents { get; private set; }
+
+//    public Money(int dollars, int cents)
+//    {
+//        Dollars = dollars;
+//        Cents = cents;
+//    }
+
+//    public void Print()
+//    {
+//        Console.WriteLine("Money: " + Dollars + '.' + Cents);
+//    }
+
+//    public void SetAmount(int dollars, int cents)
+//    {
+//        Dollars = dollars;
+//        Cents = cents;
+//    }
+//}
+
+//public class Product
+//{
+//    public string Name { get; private set; }
+//    public Money Price { get; }
+
+//    public Product(string name, Money price)
+//    {
+//        Name = name;
+//        Price = price;
+//    }
+
+//    public void ReducePrice(int dollars, int cents)
+//    {
+//        int tempDollar = Price.Dollars;
+//        int tempCents = Price.Cents;
+
+
+//        tempDollar -= dollars;
+//        tempCents -= cents;
+
+//        if (tempCents < 0)
+//        {
+//            tempDollar -= 1;
+//            tempCents += 100;
+//        }
+
+//        Price.SetAmount(tempDollar, tempCents);
+//    }
+
+//    public void AddToPrice(int dollars, int cents)
+//    {
+//        int tempDollar = Price.Dollars;
+//        int tempCents = Price.Cents;
+
+//        tempDollar += dollars;
+//        tempCents += cents;
+
+//        if (tempCents >= 100)
+//        {
+//            tempDollar += tempCents / 100;
+//            tempCents %= 100;
+//        }
+
+//        Price.SetAmount(tempDollar, tempCents);
+//    }
+//}
---
+++ September/3-2/3-2/Task2.cs
@@ -0,0 +1,46 @@
+﻿//class Device
+//{
+//    private string Name;
+//    private string Description;
+
+//    public Device(string name, string description)
+//    {
+//        this.Name = name;
+//        this.Description = description;
+//    }
+
+//    public void Sound()
+//    {
+//        Console.WriteLine(Name + " emits a device sound");
+//    }
+
+//    public void Show()
+//    {
+//        Console.WriteLine("Device: " + Name);
+//    }
+
+//    public void Desc()
+//    {
+//        Console.WriteLine("Device description: " + Description);
+//    }
+//}
+
+//class Kettle : Device
+//{
+//    public Kettle(string name, string description) : base(name, description){ }
+//}
+
+//class Microwave : Device
+//{
+//    public Microwave(string name, string description) : base(name, description) { }
+//}
+
+//class Car : Device
+//{
+//    public Car(string name, string description) : base(name, description) { }
+//}
+
+//class Ship : Device
+//{
+//    public Ship(string name, string description) : base(name, description) { }
+//}
