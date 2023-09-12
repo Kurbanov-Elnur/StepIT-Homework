b00c55f4efc2b724f76e45f5a5fc0e89cab8c4bb
Atilla <mursalzadeh99@gmail.com>
2023-09-12 12:32:33 +0400

    Add HW

---
+++ September/10/10/Task1.cs
@@ -0,0 +1,52 @@
+﻿//MyArray numbers = new();
+
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+
+//Console.WriteLine(numbers.Less(5));
+//Console.WriteLine(numbers.Greater(3));
+
+//public interface ICalc
+//{
+//    public int Less(int valueToCompare);
+
+//    int Greater(int valueToCompare);
+//}
+
+//class MyArray : ICalc
+//{
+//    private List<int> Numbers = new();
+
+//    public void Add(int Value)
+//    {
+//        Numbers.Add(Value);
+//    }
+
+//    public void Remove(int value)
+//    {
+//        Numbers.Remove(value);
+//    }
+
+//    public int Less(int ValueToCompare)
+//    {
+//        int count = 0;
+//        foreach (int item in Numbers)
+//        {
+//            if (item < ValueToCompare)
+//                count++;
+//        }
+//        return count;
+//    }
+
+//    public int Greater(int valueToCompare)
+//    {
+//        int count = 0;
+//        foreach (int item in Numbers)
+//        {
+//            if (item > valueToCompare)
+//                count++;
+//        }
+//        return count;
+//    }
+//}
---
+++ September/10/10/Task2.cs
@@ -0,0 +1,47 @@
+﻿//MyArray numbers = new();
+
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+
+//numbers.ShowEven();
+//numbers.ShowOdd();
+
+//public interface IOutput2
+//{
+//    void ShowEven();
+//    void ShowOdd();
+//}
+
+//class MyArray : IOutput2
+//{
+//    private List<int> Numbers = new();
+
+//    public void Add(int Value)
+//    {
+//        Numbers.Add(Value);
+//    }
+
+//    public void Remove(int value)
+//    {
+//        Numbers.Remove(value);
+//    }
+
+//    public void ShowEven()
+//    {
+//        foreach (int item in Numbers)
+//        {
+//            if(item % 2 == 0)
+//                Console.WriteLine(item);
+//        }
+//    }
+
+//    public void ShowOdd()
+//    {
+//        foreach (int item in Numbers)
+//        {
+//            if (item % 2 != 0)
+//                Console.WriteLine(item);
+//        }
+//    }
+//}
