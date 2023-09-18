2998bb37cc7902d1978b04c00880fdce99a26558
Atilla <mursalzadeh99@gmail.com>
2023-09-18 14:53:34 +0400

    Add HW

---
+++ September/14/14/Functions.cs
@@ -0,0 +1,20 @@
+﻿namespace BankAccountOperation
+{
+    public class Actions
+    {
+        public static double IncreaseBalance(double amount, double Balance)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public static double DecreaseBalance(double amount, double Balance)
+        {
+            if (amount > Balance)
+                throw new Exception("Not enough funds");
+
+            Balance -= amount;
+            return Balance;
+        }
+    }
+}
---
+++ September/14/14/Program.cs
@@ -0,0 +1,45 @@
+﻿BankAccount account = new("Leo", 50);
+
+account.AccountOperation = BankAccountOperation.Actions.IncreaseBalance;
+
+account.Deposit(30);
+
+account.DisplayBalance();
+
+account.AccountOperation = BankAccountOperation.Actions.DecreaseBalance;
+
+try{
+    account.Withdraw(82);
+}catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+
+account.DisplayBalance();
+
+class BankAccount
+{
+    public string AccountName;
+    public double Balance { get; set; }
+    public Func<double, double, double> AccountOperation { get; set; }
+
+    public BankAccount(string accountName, double balance)
+    {
+        this.AccountName = accountName;
+        this.Balance = balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        this.Balance = AccountOperation(amount, this.Balance);
+    }
+
+    public void Withdraw(double amount)
+    {
+        this.Balance = AccountOperation(amount, this.Balance);
+    }
+
+    public void DisplayBalance()
+    {
+        Console.WriteLine("Current balance: " + this.Balance);
+    }
+}
