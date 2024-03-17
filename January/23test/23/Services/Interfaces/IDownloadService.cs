c1d823af37460c9a1c7e6d2df68c12bfae452b28
Elnur <mursalzadeh99@gmail.com>
2024-03-17 11:33:49 +0400

    Add new branch

--- January/23test/23/Services/Interfaces/IDownloadService.cs
+++
@@ -1,6 +0,0 @@
-namespace _23.Services.Interfaces;
-
-public interface IDownloadService
-{
-    public string DownloadData(string movieName, string page);
-}
--- January/23test/23/Services/Interfaces/IJSONService.cs
+++
@@ -1,6 +0,0 @@
-namespace _23.Services.Interfaces;
-
-public interface IJSONService
-{
-    public T Deserialize<T>(string json);
-}
