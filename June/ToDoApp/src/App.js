0b727733208cfb9218e8acf1a3ff183a1cb69f11
Elnur <mursalzadeh99@gmail.com>
2024-06-03 18:07:31 +0400

    Add branch

---
+++ June/ToDoApp/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import {Test} from './test.jsx'
+
+function App() {
+  return (
+    <Test></Test>
+  );
+}
+
+export default App;
---
+++ June/ToDoApp/src/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Test() {
+    const styles = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'black',
+        display: 'inline-block'
+    };
+
+    return (
+        <div style={styles}></div>
+    )
+}
