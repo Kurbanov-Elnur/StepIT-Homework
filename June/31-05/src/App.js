03f31a6119064755362783e637574324187a8925
Elnur <mursalzadeh99@gmail.com>
2024-06-24 13:28:10 +0400

    Add HW

---
+++ June/31-05/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CountdownTimer from './components/CountdownTimer';
+import RandomQuote from './components/RandomQuote';
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer />
+      <RandomQuote />
+    </div>
+  );
+}
+
+export default App;
